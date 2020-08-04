2b964e6575fb3df27215578a4a5fc7175ba7b810
Christian Cwienk <christian.cwienk@sap.com>
2020-08-04 17:46:52 +0200

    prepare python-package-build

---
+++ bindings-python/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import os
+
+own_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+def requirements():
+    with open(os.path.join(own_dir, 'requirements.txt')) as f:
+        for line in f.readlines():
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+            yield line
+
+
+setuptools.setup(
+    name='gardener-component-model',
+    version='0.0.0-dev',
+    description='Gardener Component Model',
+    python_requires='>=3.8.*',
+    packages=setuptools.find_packages(),
+    install_requires=list(requirements()),
+    entry_points={
+    },
+)
