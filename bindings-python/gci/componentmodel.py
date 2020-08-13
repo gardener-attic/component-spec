158fd4a3a3d72daf4592e144ba0b6b40691bc633
Christian Cwienk <christian.cwienk@sap.com>
2020-08-13 15:19:01 +0200

    adjust to (again) reworked schema

--- bindings-python/gci/componentmodel.py
+++ bindings-python/gci/componentmodel.py
@@ -12,13 +12,49 @@ class SchemaVersion(enum.Enum):
     V2 = 'v2'
 
 
-class ComponentType(enum.Enum):
-    GARDENER_COMPONENT = 'gardenerComponent'
-    OCI_COMPONENT = 'ociComponent'
+class AccessType(enum.Enum):
+    OCI_REGISTRY = 'ociRegistry'
+    GITHUB = 'github'
 
+
+class SourceType(enum.Enum):
+    GIT = 'git'
+
+
+class ResourceType(enum.Enum):
     OCI_IMAGE = 'ociImage'
     WEB_DEPENDENCY = 'web'
     GENERIC = 'generic'
+
+
+@dc(frozen=True)
+class ResourceAccess:
+    type: AccessType
+
+
+@dc(frozen=True)
+class OciAccess(ResourceAccess):
+    imageReference: str
+
+
+@dc(frozen=True)
+class GithubAccess(ResourceAccess):
+    repoUrl: str
+    ref: str
+
+
+@dc(frozen=True)
+class WebAccess(ResourceAccess):
+    url: str
+
+
+class Provider(enum.Enum):
+    '''
+    internal: from repositoryContext-owner
+    external: from 3rd-party (not repositoryContext-owner)
+    '''
+    INTERNAL = 'internal'
+    EXTERNAL = 'external'
 
 
 @dc(frozen=True)
@@ -26,90 +62,59 @@ class Metadata:
     schemaVersion: SchemaVersion
 
 
+
 @dc(frozen=True)
 class ComponentReference:
     name: str
     version: str
-    type: ComponentType
 
 
 @dc(frozen=True)
-class ResolvableComponentReference(ComponentReference):
-    pass
-
-
-@dc(frozen=True)
-class DependencyComponent(ComponentReference):
-    pass
-
-
-@dc(frozen=True)
-class OciImage(DependencyComponent):
-    imageReference: str
-
-
-@dc(frozen=True)
-class WebDependency(DependencyComponent):
-    url: str
-
-
-@dc(frozen=True)
-class GenericDependency(DependencyComponent):
-    pass
-
-
-@dc
-class ResolvableComponent:
-    dependencies: typing.List[
-        typing.Union[
-            ComponentReference,
-            DependencyComponent,
-        ]
+class Resource:
+    name: str
+    version: str
+    type: ResourceType
+    access: typing.Union[
+        ResourceAccess,
+        OciAccess,
+        GithubAccess,
+        WebAccess,
     ]
 
 
 @dc
-class GardenerComponent(ResolvableComponent):
-    pass
+class RepositoryContext:
+    baseUrl: str
+    type: AccessType = AccessType.OCI_REGISTRY
 
 
 @dc
-class OciComponent(ResolvableComponent):
-    pass
+class ComponentSource:
+    name: str
+    access: typing.Union[
+        GithubAccess,
+    ]
+    type: SourceType = SourceType.GIT
 
 
 @dc
-class Overwrite:
-    componentReference: ResolvableComponentReference
-    componentOverwrites: dict = dataclasses.field(
-        default_factory=dict,
-    ) # XXX define explicit overwrite-type(s)
-    dependencyOverwrites: typing.List[
-        typing.Union[
-            ResolvableComponentReference,
-            OciImage,
-            WebDependency,
-            GenericDependency,
-        ]
-    ] = dataclasses.field(default_factory=list)
+class Component:
+    name: str    # must be valid URL w/o schema
+    version: str # relaxed semver
 
+    repositoryContexts: typing.List[RepositoryContext]
+    provider: Provider
 
-@dc
-class OverwriteDeclaration:
-    declaringComponent: ResolvableComponentReference
-    overwrites: typing.List[Overwrite]
+    sources: typing.List[ComponentSource]
+    componentReferences: typing.List[ComponentReference]
+    localResources: typing.List[Resource]
+    externalResources: typing.List[Resource]
 
 
 @dc
 class ComponentDescriptor:
     meta: Metadata
-    components: typing.List[
-        typing.Union[
-            GardenerComponent,
-            OciComponent,
-        ]
-    ]
-    overwriteDeclarations: typing.List[OverwriteDeclaration]
+    component: Component
 
     @staticmethod
     def from_dict(component_descriptor_dict: dict):
@@ -118,8 +123,11 @@ class ComponentDescriptor:
             data=component_descriptor_dict,
             config=dacite.Config(
                 cast=[
+                    AccessType,
+                    Provider,
+                    ResourceType,
                     SchemaVersion,
-                    ComponentType,
+                    SourceType,
                 ]
             )
         )
