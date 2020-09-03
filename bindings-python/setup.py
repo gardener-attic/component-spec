356cc9dc0b5315480632ff7b8d814119497b9a73
Christian Cwienk <christian.cwienk@sap.com>
2020-09-03 15:46:00 +0200

    release gear for python-bindings

--- bindings-python/setup.py
+++ bindings-python/setup.py
@@ -4,6 +4,15 @@ import os
 own_dir = os.path.abspath(os.path.dirname(__file__))
 
 
+def version():
+    if (version := os.environ.get('EFFECTIVE_VERSION')):
+        return version
+
+    print('warning: env var EFFECTIVE_VERSION not set - falling back to VERSION file')
+    with open(os.path.join(own_dir, os.pardir, 'VERSION')) as f:
+        return f.read().strip()
+
+
 def requirements():
     with open(os.path.join(own_dir, 'requirements.txt')) as f:
         for line in f.readlines():
@@ -15,7 +24,7 @@ def requirements():
 
 setuptools.setup(
     name='gardener-component-model',
-    version='0.0.0alpha8',
+    version=version(),
     description='Gardener Component Model',
     python_requires='>=3.8.*',
     packages=setuptools.find_packages(),
