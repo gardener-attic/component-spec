0d2052ae7c056e112cfa2472400e35d5da9d8509
Christian Cwienk <christian.cwienk@sap.com>
2020-10-27 10:51:26 +0100

    fix: cast enum

--- bindings-python/gci/componentmodel.py
+++ bindings-python/gci/componentmodel.py
@@ -193,6 +193,7 @@ class ComponentDescriptor:
                     ResourceType,
                     SchemaVersion,
                     SourceType,
+                    ResourceRelation,
                 ]
             )
         )
