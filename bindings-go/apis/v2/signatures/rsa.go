8aa01b967b4e53b034a35625896efcdbce112640
enrico-kaack-comp <enrico.kaack@sap.com>
2021-11-04 10:59:27 +0100

    removed unused hash algorithms and todos

--- bindings-go/apis/v2/signatures/rsa.go
+++ bindings-go/apis/v2/signatures/rsa.go
@@ -50,7 +50,7 @@ func (s RsaSigner) Sign(componentDescriptor v2.ComponentDescriptor, digest v2.Di
 		return nil, fmt.Errorf("failed signing hash, %w", err)
 	}
 	return &v2.SignatureSpec{
-		Algorithm: "RSASSA-PKCS1-V1_5-SIGN", //TODO: check
+		Algorithm: "RSASSA-PKCS1-V1_5-SIGN",
 		Data:      hex.EncodeToString(signature),
 	}, nil
 }
@@ -60,12 +60,6 @@ func hashAlgorithmLookup(algorithm string) (crypto.Hash, error) {
 	switch strings.ToUpper(algorithm) {
 	case "SHA256":
 		return crypto.SHA256, nil
-	case "SHA512":
-		return crypto.SHA512, nil //TODO: test
-	case "SHA3_256":
-		return crypto.SHA3_256, nil // TODO: test
-	case "SHA3_512":
-		return crypto.SHA3_512, nil // TODO: test
 	}
 	return 0, fmt.Errorf("hash Algorithm %s not found", algorithm)
 }
