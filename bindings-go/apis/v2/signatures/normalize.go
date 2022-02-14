f31d5ff93c0f6ed8131aba2372c278d17db264b9
enrico-kaack-comp <enrico.kaack@sap.com>
2022-02-14 17:01:11 +0100

    allow missing res digests in normailsation

--- bindings-go/apis/v2/signatures/normalize.go
+++ bindings-go/apis/v2/signatures/normalize.go
@@ -60,7 +60,7 @@ func HashForComponentDescriptor(cd v2.ComponentDescriptor, hash Hasher) (*v2.Dig
 }
 
 func normalizeComponentDescriptor(cd v2.ComponentDescriptor) ([]byte, error) {
-	if err := isNormaliseable(cd); err != nil {
+	if err := isNormaliseableUnsafe(cd); err != nil {
 		return nil, fmt.Errorf("can not normalise component-descriptor %s:%s: %w", cd.Name, cd.Version, err)
 	}
 
@@ -93,6 +93,17 @@ func normalizeComponentDescriptor(cd v2.ComponentDescriptor) ([]byte, error) {
 
 		//ignore access.type=None for normalisation and hash calculation
 		if res.Access.Type == "None" {
+			resource := []Entry{
+				{"name": res.Name},
+				{"version": res.Version},
+				{"extraIdentity": extraIdentity},
+			}
+			resources = append(resources, resource)
+			continue
+		}
+
+		//ignore a resource without digests
+		if res.Digest == nil {
 			resource := []Entry{
 				{"name": res.Name},
 				{"version": res.Version},
@@ -228,6 +239,18 @@ func isNormaliseable(cd v2.ComponentDescriptor) error {
 		}
 		if res.Digest == nil || res.Digest.HashAlgorithm == "" || res.Digest.NormalisationAlgorithm == "" || res.Digest.Value == "" {
 			return fmt.Errorf("missing digest in resource for %s:%s", res.Name, res.Version)
+		}
+	}
+	return nil
+}
+
+// isNormaliseableUnsafe checks if componentReferences contain digest. It does not check resources for containing digests.
+// Does NOT verify if the digests are correct
+func isNormaliseableUnsafe(cd v2.ComponentDescriptor) error {
+	// check for digests on component references
+	for _, reference := range cd.ComponentReferences {
+		if reference.Digest == nil || reference.Digest.HashAlgorithm == "" || reference.Digest.NormalisationAlgorithm == "" || reference.Digest.Value == "" {
+			return fmt.Errorf("missing digest in componentReference for %s:%s", reference.Name, reference.Version)
 		}
 	}
 	return nil
