a46231ed9e440d8a48b9431dca9e9ef559739679
enrico-kaack-comp <enrico.kaack@sap.com>
2023-04-11 16:25:43 +0200

    update golangcli

--- bindings-go/hack/install-requirements.sh
+++ bindings-go/hack/install-requirements.sh
@@ -19,5 +19,5 @@ set -e
 echo "> Install requirements"
 
 GO111MODULE=off go get golang.org/x/tools/cmd/goimports
-GO111MODULE=on curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.27.0
+GO111MODULE=on curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.52.2
 GO111MODULE=off go get -u github.com/go-bindata/go-bindata/...
