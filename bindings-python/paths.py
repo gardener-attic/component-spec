1056ea839de17e02221ff1a48ed436115d686d5c
Christian Cwienk <christian.cwienk@sap.com>
2023-09-23 09:28:13 +0200

    rm python-bindings (moved to github.com/gardener/cc-utils)

--- bindings-python/paths.py
+++
@@ -1,6 +0,0 @@
-import os
-
-own_dir = os.path.abspath(os.path.dirname(__file__))
-repo_root = os.path.abspath(os.path.join(own_dir, os.pardir))
-indep_dir = os.path.join(repo_root, 'language-independent')
-test_res_dir = os.path.join(indep_dir, 'test-resources')
